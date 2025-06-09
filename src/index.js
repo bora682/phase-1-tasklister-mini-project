102fd11b3aa4466793d4a4748ba104fbd1744aac
Bora <bora@christinesmbp16.mynetworksettings.com>
2025-06-09 17:57:37 -0400

    Complete Tasklister lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const taskInput = event.target.elements["new-task-description"].value;
+    buildToDo(taskInput);
+    event.target.reset();
+  });
+  
   // your code here
+function buildToDo(task) {
+  const taskList = document.getElementById("tasks");
+  const li = document.createElement("li");
+  li.textContent = task;
+  taskList.appendChild(li);
+}
+
 });
